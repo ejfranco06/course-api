55a92dfee95d9323b3b42dd7e111e94b0f0a208e
Emilio Franco <ejfranco06@gmail.com>
2020-06-15 10:38:11 -0400

    Initial commit

---
+++ src/main/java/dev/emiliofranco/courseapi/topic/TopicController.java
@@ -0,0 +1,21 @@
+package dev.emiliofranco.courseapi.topic;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class TopicController {
+
+    @GetMapping("/topics")
+    public List<Topic> getAllTopics(){
+
+        return Arrays.asList(
+                new Topic("spring", "Spring Framework", "Spring Framework Description"),
+                new Topic("java", "Core Java", "Core Java Description"),
+                new Topic("javascript", "JavaScript", "JavaScript Description")
+        );
+    }
+}
